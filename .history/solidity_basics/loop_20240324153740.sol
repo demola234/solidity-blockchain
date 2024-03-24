fb041bad814be756cced274609ca1125d5a82bc6
demola234 <kolawoleoluwasegun567@gmail.com>
2024-03-24 18:38:49 +0100

    using loops and pure function

---
+++ .history/solidity_basics/loop_20240324153740.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract ExploringLoops {
+    function countUp(uint limit) public pure returns (uint) {
+        uint count = 0;
+        for 
+    }
+}
